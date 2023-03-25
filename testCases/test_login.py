428a57e5320d141e3c89efefc84aa5d36443a995
Jeffrey <trainmyselfonline@gmail.com>
2023-03-26 06:01:28 +0800

    adding allure report functionality in test_login.py

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,19 +1,23 @@
 import pytest
+from allure_commons.types import AttachmentType
 from selenium import webdriver
 from pageObjects.LoginPage import LoginPage
 from utilities.readProperties import ReadConfig
 from utilities.customLogger import LogGen
+import allure
+
 
 # To run the test by group
 # pytest -v -s -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
 
+@allure.severity(allure.severity_level.NORMAL)
 class Test_001_Login:
     baseURL = ReadConfig.getApplicationURL()
     username = ReadConfig.getUseremail()
     password = ReadConfig.getPassword()
     logger = LogGen.loggen()
 
-
+    @allure.severity(allure.severity_level.MINOR)
     @pytest.mark.regression
     def test_homePageTitle(self, setup):
         self.logger.info("********** Test_001_Login ********")
@@ -26,11 +30,14 @@ class Test_001_Login:
             self.driver.close()
             self.logger.info("********** Home Page title is PASSED ********")
         else:
+            allure.attach(self.driver.get_screenshot_as_png(), name="testTitlePage",
+                          attachment_type=AttachmentType.PNG)
             self.driver.save_screenshot(filename=".//Screenshots//" + "test_homePageTitle.png")
             self.driver.close()
             self.logger.error("********** Home Page title is FAILED ********")
             assert False, f"Invalid title: {act_title}"
 
+    @allure.severity(allure.severity_level.CRITICAL)
     @pytest.mark.sanity
     @pytest.mark.regression
     def test_login(self, setup):
@@ -48,9 +55,9 @@ class Test_001_Login:
             self.driver.close()
             assert True
         else:
+            allure.attach(self.driver.get_screenshot_as_png(), name="testLoginScreen",
+                          attachment_type=AttachmentType.png)
             self.driver.save_screenshot(filename=".//Screenshots//" + "test_login.png")
             self.logger.error("********** Login test FAILED ********")
             self.driver.close()
             assert False, f"Invalid title {act_title}"
-
-
